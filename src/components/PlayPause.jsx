3c566ef24ab732afd363e057d3b063ba8813651a
aaryan200 <co21btech11001@iith.ac.in>
2023-06-05 17:06:55 +0530

    First Commit

---
+++ src/components/PlayPause.jsx
@@ -0,0 +1,18 @@
+import { BsPlayCircle, BsPauseCircle } from 'react-icons/bs';
+
+
+const PlayPause = ({isPlaying, activeSong, song, handlePause, handlePlay}) => (
+  isPlaying && activeSong?.title === song.title ? 
+  (
+  <BsPauseCircle size={35} className="text-gray-300"
+  onClick={handlePause}
+  />
+  ) : 
+  (
+  <BsPlayCircle size={35} className="text-gray-300"
+  onClick={handlePlay}
+  />
+  )
+);
+
+export default PlayPause;
